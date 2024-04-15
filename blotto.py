74db662bbc2c58d25d986276d4d08969bc8838f7
Ganesh Murugappan <murugappan.ganesh@gmail.com>
2024-04-15 00:38:07 -0400

    Add blotto submissions

--- blotto.py
+++ blotto.py
@@ -1,9 +1,12 @@
-setups = [[10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-          [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-          [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-          [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-          [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-          [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]
+setups = [[ 1,  1,  8,  1,  1,  1,  4, 21, 31, 31],
+          [ 0, 10, 14,  0, 12, 14, 16, 16, 18,  0],
+          [  3, 0, 11,  0, 18,  0, 35,  0, 33,  0],
+          [ 0,  0,  6,  8, 11, 14, 17, 20, 24,  0],
+          [ 0,  5,  6,  9, 12,  1, 27, 31,  2,  7],
+          [ 1,  1,  1,  2, 16, 21, 21, 26, 10,  1]]
+
+
+names = ["The Banglas", "Gujju Gang", "Korndapalli Farm Friends", "Dream Team", "Swaggy MAPY", "Triple-A"]
 
 for i, setup in enumerate(setups):
     if sum(setup) != 100:
@@ -27,4 +30,4 @@ for i in range(len(setups)-1):
             results[i] += 1
 
 for i, res in enumerate(results):
-    print(f"Team {i+1}: {res}")
+    print(f"Team {names[i]}: {res}")
